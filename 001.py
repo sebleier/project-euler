48c0cb4880c126e4003e17a8a92f83a3d5a5618f
Sean Bleier <sebleier@gmail.com>
2009-08-17 20:52:55 -0500

    remove some unused stuff

--- 001.py
+++ 001.py
@@ -3,17 +3,7 @@
         If we list all the natural numbers below 10 that are multiples of 3 or 
         5, we get 3, 5, 6 and 9. The sum of these multiples is 23.
         
-        Find the sum of all the multiples of 3 or 5 below 1000.\
-        
-    Solution:
-        
-        
-        n(n + 1) / 2 => 1 + 2 + 3 + ... + n
-        
-        3 + 6 + 9 + 12 + 15 = 45 
-        5 + 10 + 15 = 30
-        == 75
-         
+        Find the sum of all the multiples of 3 or 5 below 1000.                 
 """
 ceiling = 999
 nums = (3, 5)
